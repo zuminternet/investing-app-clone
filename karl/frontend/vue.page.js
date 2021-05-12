9cdcecae5832fd7e582238c531a7014728490924
gitgitWi <johnwi@knou.ac.kr>
2021-05-12 17:40:16 +0900

    Build: 파일구조 변경
    - common + 각자 폴더
    - common에서는 공통적으로 사용할 부분 구현
    - 각자 폴더에서 담당 부분 구현
    
    ## TODO
    
    - 현재 설정파일들이 common 디렉토리 못찾고 있어서 실행 불가

---
+++ karl/frontend/vue.page.js
@@ -0,0 +1,10 @@
+module.exports = {
+    index: { // 모바일 웹 메인
+        entry: 'frontend/main.js',
+        ssrEntry: 'frontend/ssr_main.js',
+        template: 'templates/index.html', // 원본 템플릿 파일
+        // publishTemplate: 'stub/index.html', // 퍼블리시 모드에서 사용할 템플릿 파일
+        filename: 'templates/dist/index.html', // 빌드 후 템플릿 파일
+        path: ['/*'] // dev 모드 프록시에 적용할 path
+    },
+};
---
+++ wiii/frontend/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
