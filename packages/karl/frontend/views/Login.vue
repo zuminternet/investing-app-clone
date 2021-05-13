2332bda64bd81074ebfcd8198b40df81394c6eda
Jaemin Cheon <karl@zuminternet.com>
2021-05-13 15:29:04 +0900

    [chore] metviewport 등 meta tags 추가

---
+++ packages/karl/frontend/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Login</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style></style>
---
+++ packages/karl/frontend/views/index.ts
@@ -0,0 +1,4 @@
+import Home from './Home.vue'
+import Login from './Login.vue'
+
+export { Home, Login }
