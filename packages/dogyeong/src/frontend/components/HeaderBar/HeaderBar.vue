ac425a256385230730b08a4243a0146e64aee1ea
dogyeong <dogyeong@zuminternet.com>
2021-05-17 18:28:08 +0900

    Feat: Apply style variables

--- packages/dogyeong/src/frontend/components/HeaderBar/HeaderBar.vue
+++ packages/dogyeong/src/frontend/components/HeaderBar/HeaderBar.vue
@@ -12,11 +12,12 @@ export default {
 
 <style lang="scss" scoped>
 header {
-  background-color: darkblue;
+  background-color: var(--header-bg-color);
   width: 100%;
   height: 48px;
   padding: 6px 12px;
   display: flex;
   align-items: center;
+  border-bottom: 1px solid var(--app-bg-color);
 }
 </style>
