176c7bf9699cf7a4c389a8c1b13842a72339358f
dogyeong <dogyeong@zuminternet.com>
2021-05-18 18:22:21 +0900

    Refactor: Remove unused method

--- packages/dogyeong/src/backend/service/AuthService.ts
+++ packages/dogyeong/src/backend/service/AuthService.ts
@@ -7,25 +7,6 @@ export default class AuthService {
   constructor(@Yml('oauthConfig') private OAuthConfig) {}
 
   /**
-   * 사용자를 인증 동의 페이지로 리다이렉트 시키기 위한 url을 반환
-   */
-  public getAuthRedirectionUrl() {
-    const {
-      googleOAuthConfig: { baseUrl, scope, responseType, redirectUrl, clientId },
-    } = this.OAuthConfig;
-
-    const OAuthUrl =
-      `${baseUrl}?` +
-      `scope=${scope}&` +
-      `include_granted_scopes=true&` +
-      `response_type=${responseType}&` +
-      `redirect_uri=${redirectUrl}&` +
-      `client_id=${clientId}`;
-
-    return OAuthUrl;
-  }
-
-  /**
    * 사용자의 동의를 받고 얻은 코드로 사용자 정보를 받아와서 반환
    * @param code
    * @returns userInfo | null
