617d50177aa39ad8ae1ee4a08da639a7d7640807
gitgitWi <johnwi@knou.ac.kr>
2021-05-18 10:22:41 +0000

    Refactor: Header component, atomic pattern으로 변경
    - component 구조
      - 기능별 구조에서 atomic pattern으로 변경
      - `HomeHeader` component 변경
    - color palette
      - material UI colorset으로 변경

---
+++ packages/wiii/frontend/components/atoms/RouterLink.vue
@@ -0,0 +1,66 @@
+<template>
+  <router-link :to="href" class="router-link" :class="isDarkTheme">{{ name }}</router-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+    },
+    isDark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    isDarkTheme() {
+      return this.isDark ? `dark` : ``;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.router-link {
+  @extend .card;
+
+  padding: 5px 15px;
+  margin: 5px;
+
+  color: $blue-500;
+  text-decoration: none;
+  transition: all 0.08s ease-in-out;
+
+  &:hover {
+    color: $grey-900;
+    background-color: $blue-300;
+  }
+
+  &.dark {
+    color: $grey-100;
+    text-shadow: 0 0 15px $red-neon;
+    background-color: $grey-700;
+    box-shadow: 0 0 10px $red-neon;
+
+    &:hover {
+      background-color: $grey-500;
+      color: $red-neon;
+      font-weight: bold;
+    }
+  }
+
+  &-active,
+  &-exact-active {
+    &.dark {
+      color: $red-neon;
+    }
+  }
+}
+</style>
---
+++ packages/wiii/frontend/components/molecules/HomeTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <Title :titleText="titleText" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Title from '@/components/atoms/Title';
+import { viewsTitle } from '@/type/views';
+
+export default Vue.extend({
+  name: 'HomeTitle',
+
+  components: {
+    Title,
+  },
+
+  computed: {
+    titleText() {
+      return viewsTitle.Home;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
