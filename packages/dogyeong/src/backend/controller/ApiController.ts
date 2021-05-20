b53c40c5459f0dc2a50084f57bf6a5ebabbced85
dogyeong <dogyeong@zuminternet.com>
2021-05-20 16:38:23 +0900

    Feat: Apply TokenService to ApiController
    * TokenService 적용하여 jwt를 로그인/회원가입 응답으로 전달

--- packages/dogyeong/src/backend/controller/ApiController.ts
+++ packages/dogyeong/src/backend/controller/ApiController.ts
@@ -3,31 +3,31 @@ import { Request, Response } from 'express';
 import { Inject } from 'zum-portal-core/backend/decorator/Alias';
 import AuthService from '../service/AuthService';
 import UserService from '../service/UserService';
+import TokenService from '../service/TokenService';
 
 @Controller({ path: '/api' })
 export class ApiController {
-  constructor(@Inject(AuthService) private authService: AuthService, @Inject(UserService) private userService: UserService) {}
+  constructor(
+    @Inject(AuthService) private authService: AuthService,
+    @Inject(UserService) private userService: UserService,
+    @Inject(TokenService) private tokenService: TokenService,
+  ) {}
 
   @GetMapping({ path: ['/auth/google'] })
   public async loginGoogleUser(req: Request, res: Response) {
     const code = req.headers['inv_google_auth'];
 
-    if (!code) {
-      return res.status(400).json({ message: 'Invalid Header' });
-    }
+    if (!code) return res.status(400).json({ message: 'Invalid Header' });
 
     const userInfo = await this.authService.getUserInfo(code);
 
-    if (!userInfo) {
-      return res.json({});
-    }
+    if (!userInfo) return res.json({});
 
-    // 가입 안되어있으면 계정 생성하고, 유저 정보 반환
+    // 가입 안되어있으면 계정을 생성하고, 유저 정보 반환
     const user = await this.userService.createGoogleUser(userInfo);
+    const token = this.tokenService.createToken(user);
 
-    // create jwt
-
-    res.json({ user });
+    res.json({ token, user });
   }
 
   @PostMapping({ path: ['/user'] })
@@ -48,7 +48,9 @@ export class ApiController {
 
       if (!user) return res.status(400).send('Email or Password is incorrect');
 
-      res.json({ user });
+      const token = this.tokenService.createToken(user);
+
+      res.json({ token, user });
     } catch (err) {
       res.status(500).json({ err });
     }
