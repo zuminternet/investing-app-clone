ebd75f786a7067de6d7b419834c5f58e521fb35a
dogyeong <dogyeong@zuminternet.com>
2021-05-20 15:43:32 +0900

    Feat: Add mongoose connect logic
    * 서버 실행 시 몽구스 연결 구현

--- packages/dogyeong/src/backend/Server.ts
+++ packages/dogyeong/src/backend/Server.ts
@@ -1,4 +1,17 @@
 import 'reflect-metadata';
 import { appContainer } from 'common/backend/AppContainer';
+import * as mongoose from 'mongoose';
 
-appContainer.listen();
+mongoose
+  .connect('mongodb://127.0.0.1:27017/investing', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: 'zum',
+    pass: 'zumdev',
+    authSource: 'admin',
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    appContainer.listen();
+  })
+  .catch((e) => console.log(e));
