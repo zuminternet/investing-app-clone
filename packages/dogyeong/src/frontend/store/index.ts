63bf2ce089e501175f5f46118aa01e96edad2a8d
dogyeong <dogyeong@zuminternet.com>
2021-05-21 13:11:08 +0900

    Feat: Add actions to store
    * 로그인,로그아웃,유저 데이터 받아오는 비동기 로직을 수행하는 actions
      추가

--- packages/dogyeong/src/frontend/store/index.ts
+++ packages/dogyeong/src/frontend/store/index.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Axios from 'axios';
 
 Vue.use(Vuex);
+
+const GOOGLE_AUTH_OPTIONS = {
+  prompt: 'select_account',
+};
 
 export default () => {
   return new Vuex.Store({
@@ -23,6 +28,51 @@ export default () => {
       },
     },
 
-    actions: {},
+    actions: {
+      async login({ commit }, { email, password }) {
+        try {
+          const {
+            data: { user },
+          } = await Axios.post('/api/auth', { email, password });
+          console.log(user);
+          commit('setUser', user.name);
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      async googleLogin({ commit }) {
+        try {
+          const gapi = (window as any).gapi;
+          const { code } = await gapi.auth2.getAuthInstance().grantOfflineAccess(GOOGLE_AUTH_OPTIONS);
+
+          const {
+            data: { user },
+          } = await Axios.get('/api/auth/google', { headers: { inv_google_auth: code } });
+          console.log(user);
+          commit('setUser', user.name);
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      async fetchCurrentUser({ commit }) {
+        try {
+          const {
+            data: { user },
+          } = await Axios.get('/api/user');
+
+          commit('setUser', user.name);
+        } catch (e) {
+          commit('setUser', null);
+        }
+      },
+      async logout({ commit }) {
+        try {
+          await Axios.get('/api/logout');
+          commit('setUser', null);
+        } catch (e) {
+          console.error(e);
+        }
+      },
+    },
   });
 };
