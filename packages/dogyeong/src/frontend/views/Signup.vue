fd7122d5d6ef8d1aa17c381a9e42de392ff43031
dogyeong <dogyeong@zuminternet.com>
2021-05-21 09:44:16 +0900

    Feat: Add Signup view

---
+++ packages/dogyeong/src/frontend/views/Signup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <HeaderBar>회원가입</HeaderBar>
+    <form>
+      <label>name <input v-model="name" type="text"/></label>
+      <label>email <input v-model="email" type="email"/></label>
+      <label>password <input v-model="password" type="password"/></label>
+      <button type="submit" @click.prevent="onClickSignupBtn">sign up</button>
+    </form>
+    <BottomNav></BottomNav>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Axios from 'axios';
+import HeaderBar from '@/components/HeaderBar/HeaderBar.vue';
+import BottomNav from '@/components/BottomNav/BottomNav.vue';
+
+export default Vue.extend({
+  components: { HeaderBar, BottomNav },
+
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    onClickSignupBtn() {
+      Axios.post('/api/user', {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      }).then(() => this.$router.replace('/login'));
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
