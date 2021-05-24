f06392a3e0653358e5060c28b1450469bc392e40
dogyeong <dogyeong@zuminternet.com>
2021-05-24 16:37:03 +0900

    Refactor: Add types in financeService

--- packages/dogyeong/src/frontend/services/financeService.ts
+++ packages/dogyeong/src/frontend/services/financeService.ts
@@ -1,27 +1,44 @@
 import axios from 'axios';
 import { apiEndpoints } from '@/config';
+import { InvestingApiResponse, CandleChartData, SummaryDetail } from '../../backend/service/MarketService';
 
-export const getIndices = async () => {
+interface IndicesResponse {
+  indices: InvestingApiResponse[];
+}
+
+interface CoinsResponse {
+  coins: InvestingApiResponse[];
+}
+
+interface StocksResponse {
+  stocks: InvestingApiResponse[];
+}
+
+interface ChartResponse {
+  chart: CandleChartData[];
+}
+
+export const getIndices = async (): Promise<IndicesResponse> => {
   const { data } = await axios.get(apiEndpoints.getIndices);
   return data;
 };
 
-export const getCoins = async () => {
+export const getCoins = async (): Promise<CoinsResponse> => {
   const { data } = await axios.get(apiEndpoints.getCoins);
   return data;
 };
 
-export const getStocks = async () => {
+export const getStocks = async (): Promise<StocksResponse> => {
   const { data } = await axios.get(apiEndpoints.getStocks);
   return data;
 };
 
-export const getSummary = async () => {
+export const getSummary = async (): Promise<SummaryDetail> => {
   const { data } = await axios.get(apiEndpoints.getSummary);
   return data;
 };
 
-export const getChart = async () => {
+export const getChart = async (): Promise<ChartResponse> => {
   const { data } = await axios.get(apiEndpoints.getChart);
   return data;
 };
