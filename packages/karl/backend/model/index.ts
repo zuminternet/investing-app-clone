ada1dbaba371c905430d46fafea90c961113a160
Jaemin Cheon <karl@zuminternet.com>
2021-05-24 17:54:54 +0900

    [feature] : User 모델 정의

---
+++ packages/karl/backend/model/index.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose'
+
+const userSchema = new Schema({
+  name: String,
+  password: String,
+  email: String
+})
+
+const User = model('User', userSchema)
+
+
+
+export { User}
