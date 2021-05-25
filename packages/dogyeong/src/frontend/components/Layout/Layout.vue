ab91b4c354319d718973df5e6f3c79b7dcceddd2
dogyeong <dogyeong@zuminternet.com>
2021-05-25 14:07:48 +0900

    Feat: Add Layout component
    * 여러 페이지에서 공통적으로 들어갈 레이아웃을 잡아줄 컴포넌트

---
+++ packages/dogyeong/src/frontend/components/Layout/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout"><slot></slot></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  main {
+    overflow: hidden;
+  }
+}
+</style>
