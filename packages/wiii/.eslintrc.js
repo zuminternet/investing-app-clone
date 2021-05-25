bb7d0e0f01fda86e6eaa5e757744c7960bfdcd5c
gitgitWi <johnwi@knou.ac.kr>
2021-05-25 20:00:06 +0900

    Chore: vue-eslint plugin 추가

--- packages/wiii/.eslintrc.js
+++ packages/wiii/.eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
   root: false,
   extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/essential',
     '@vue/typescript/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
-    'prettier',
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     sourceType: 'module',
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-require-imports': false,
   },
 };
