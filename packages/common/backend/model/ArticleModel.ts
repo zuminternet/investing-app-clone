77e4e15effa0945771fc2274a31320cee5f67018
dogyeong <dogyeong@zuminternet.com>
2021-05-31 14:11:05 +0900

    Refactor: Update ArticleModel
    * 인터페이스 추가
    * enum 추가
    
    -

--- packages/common/backend/model/ArticleModel.ts
+++ packages/common/backend/model/ArticleModel.ts
@@ -2,6 +2,21 @@ import mongoose from 'common/backend/mongoose';
 
 const { Schema, model } = mongoose;
 
+export const enum ArticleType {
+  news = 'news',
+  opinions = 'opinions',
+}
+
+export interface IArticle extends mongoose.Document {
+  image_url: string;
+  title: string;
+  text: string;
+  source: string;
+  date: Date;
+  tickers: string[];
+  type: ArticleType;
+}
+
 /**
  * @description 뉴스, 의견 데이터를 위한 모델 스키마
  */
@@ -12,9 +27,13 @@ const ArticleSchema = new Schema({
   source: { type: String, required: true },
   date: { type: Date, required: true },
   tickers: [String],
-  type: { type: String, enum: ['news', 'opinions'], default: 'news' },
+  type: {
+    type: String,
+    enum: [ArticleType.news, ArticleType.opinions],
+    default: ArticleType.news,
+  },
 });
 
-const Article = model('Articles', ArticleSchema);
+const Article = model<IArticle>('Articles', ArticleSchema);
 
 export default Article;
