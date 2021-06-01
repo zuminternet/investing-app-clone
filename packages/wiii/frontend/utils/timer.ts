53350540f64063649357a556e0a8caab58c06dc5
gitgitWi <johnwi@knou.ac.kr>
2021-06-01 17:16:43 +0900

    Fix: timer 함수에 args 넘기고 return 받을 수 있도록 수정

--- packages/wiii/frontend/utils/timer.ts
+++ packages/wiii/frontend/utils/timer.ts
@@ -1,6 +1,7 @@
-export default (fn: Function, title: string) => {
+export default (fn: Function, title: string) => (...args) => {
   console.info(`[TIMER] ${title}`);
   console.time(title);
-  fn();
+  const res = fn(...args);
   console.timeEnd(title);
+  return res;
 };
