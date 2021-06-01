006839d423b71a8744d1ffacf45d6c01d6216d9b
dogyeong <dogyeong@zuminternet.com>
2021-06-01 15:43:33 +0900

    Feat: Add NewsDetail view
    -

---
+++ packages/dogyeong/src/frontend/views/NewsDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <Header>
+      <HeaderTitle>디테일</HeaderTitle>
+    </Header>
+    <main></main>
+    <BottomNav></BottomNav>
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BottomNav from '@/components/BottomNav/BottomNav.vue';
+import { Header, HeaderTitle } from '@/components/Header';
+import Layout from '@/components/Layout/Layout.vue';
+
+export default Vue.extend({
+  name: 'NewsDetail',
+
+  components: {
+    BottomNav,
+    Header,
+    HeaderTitle,
+    Layout,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
