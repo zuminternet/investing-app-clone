819c7316cec5fb61bcea3e7f71f7951c9981c753
Jaemin Cheon <karl@zuminternet.com>
2021-06-02 17:23:29 +0900

    [feature] 북마크를 위한 모델 스키마 정의

---
+++ packages/common/backend/model/BookmarkModel.ts
@@ -0,0 +1,21 @@
+import mongoose from 'common/backend/mongoose';
+
+const { Schema, model } = mongoose;
+
+export interface BookmarkDocument extends mongoose.Document {
+  symbol: string;
+  email: string;
+}
+
+/**
+ * @description bookmark 데이터를 위한 모델 스키마
+ */
+
+const BookmarkSchema = new Schema({
+  symbol: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const Bookmark = model<BookmarkDocument>('Bookmarks', BookmarkSchema);
+
+export default Bookmark;
