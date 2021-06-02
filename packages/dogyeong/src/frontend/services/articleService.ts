240357efcccdfdd778cb5bcf45a49de1273bd57f
dogyeong <dogyeong@zuminternet.com>
2021-06-02 14:12:18 +0900

    Feat: Add more news, opinions button
    * 뉴스 탭에서 더보기 기능 구현
    
    -

--- packages/dogyeong/src/frontend/services/articleService.ts
+++ packages/dogyeong/src/frontend/services/articleService.ts
@@ -8,28 +8,33 @@ interface Query {
   limit?: number;
 }
 
-export const getNewNews = async ({ offset = 0, limit = 10 }: Query = {}) => {
+export const getArticle = async (id: string) => {
+  const { data } = await Axios.get(`${apiEndpoints.getArticle}/${id}`);
+  return data;
+};
+
+export const getNewNews = async ({ offset = 0, limit = 5 }: Query = {}) => {
   const { data } = await Axios.get(apiEndpoints.getNewNews, {
     params: { offset, limit },
   });
   return data;
 };
 
-export const getNewOpinions = async ({ offset = 0, limit = 10 }: Query = {}) => {
+export const getNewOpinions = async ({ offset = 0, limit = 5 }: Query = {}) => {
   const { data } = await Axios.get(apiEndpoints.getNewOpinions, {
     params: { offset, limit },
   });
   return data;
 };
 
-export const getPopularNews = async ({ offset = 0, limit = 10 }: Query = {}) => {
+export const getPopularNews = async ({ offset = 0, limit = 5 }: Query = {}) => {
   const { data } = await Axios.get(apiEndpoints.getPopularNews, {
     params: { offset, limit },
   });
   return data;
 };
 
-export const getPopularOpinions = async ({ offset = 0, limit = 10 }: Query = {}) => {
+export const getPopularOpinions = async ({ offset = 0, limit = 5 }: Query = {}) => {
   const { data } = await Axios.get(apiEndpoints.getPopularOpinions, {
     params: { offset, limit },
   });
