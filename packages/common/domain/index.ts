d0ccb86e61a03f6ca0be793849d794efcedadebb
Lee Dogyeong <shroad1802@naver.com>
2021-06-03 15:24:18 +0900

    Feat: Add common domain types
    * 공통적으로 사용할 마켓 탭 부분 타입들 정의
    
    -

--- packages/common/domain/index.ts
+++ packages/common/domain/index.ts
@@ -1,41 +1,121 @@
-// 지수, 주식, 가상화폐
+// 지수, 주식, 가상화폐 tickers
 export const tickerMap = {
   index: {
-    IXIC: {
-      investing: 'indices/nq-100',
+    // 실제 api에서 사용되는 ticker
+    FB: {
+      //  화면에 표시될 종목 이름
       name: '나스닥100',
     },
-    DJI: {
-      investing: 'indices/us-30',
+    ADBE: {
       name: '다우존스30',
     },
-    SPX: {
-      investing: 'indices/us-spx-500',
+    ADI: {
       name: 'S&P500',
+    },
+    ADP: {
+      name: 'foo index',
+    },
+    ADSK: {
+      name: 'bar index',
     },
   },
   crypto: {
-    BTC: {
-      investing: 'crypto/bitcoin/btc-usd',
+    BIDU: {
       name: '비트코인',
     },
-    ETH: {
-      investing: 'crypto/ethereum/eth-usd?c997650',
+    EBAY: {
       name: '이더리움',
+    },
+    INTC: {
+      name: 'foo coin',
+    },
+    MAR: {
+      name: 'bar coin',
+    },
+    MNST: {
+      name: 'baz coin',
     },
   },
   stock: {
-    APPL: {
-      investing: 'equities/apple-computer-inc',
+    AAPL: {
       name: '애플',
     },
     GOOG: {
-      investing: 'equities/google-inc',
       name: '구글',
     },
     TSLA: {
-      investing: 'equities/tesla-motors',
       name: '테슬라',
+    },
+    AMZN: {
+      name: '아마존',
+    },
+    PYPL: {
+      name: '페이팔',
     },
   },
 } as const;
+
+export type IndexSymbol = keyof typeof tickerMap.index;
+
+export type CryptoSymbol = keyof typeof tickerMap.crypto;
+
+export type StockSymbol = keyof typeof tickerMap.stock;
+
+export type MarketSymbol = IndexSymbol | CryptoSymbol | StockSymbol;
+
+// 종목상세 : 52주 변동폭, 일일 변동폭, 거래량(volume), 시가총액(marketcap), 매수가(bid), 매도가(ask),
+export interface SummaryDetail {
+  maxAge: number;
+  priceHint: number;
+  previousClose: number;
+  open: number;
+  dayLow: number;
+  dayHigh: number;
+  regularMarketPreviousClose: number;
+  regularMarketOpen: number;
+  regularMarketDayLow: number;
+  regularMarketDayHigh: number;
+  dividendRate: number;
+  dividendYield: number;
+  exDividendDate: Date;
+  payoutRatio: number;
+  fiveYearAvgDividendYield: number;
+  beta: number;
+  trailingPE: number;
+  forwardPE: number;
+  volume: number;
+  regularMarketVolume: number;
+  averageVolume: number;
+  averageVolume10days: number;
+  averageDailyVolume10Day: number;
+  bid: number;
+  ask: number;
+  bidSize: number;
+  askSize: number;
+  marketCap: number;
+  fiftyTwoWeekLow: number;
+  fiftyTwoWeekHigh: number;
+  priceToSalesTrailing12Months: number;
+  fiftyDayAverage: number;
+  twoHundredDayAverage: number;
+  trailingAnnualDividendRate: number;
+  trailingAnnualDividendYield: number;
+}
+
+export interface MarketStockEOD {
+  date: Date;
+  high: number;
+  low: number;
+  open: number;
+  close: number;
+  symbol: MarketSymbol;
+}
+
+export interface EndOfDay extends MarketStockEOD {
+  display_name: string;
+}
+
+export interface HistoricalData {
+  display_name: string;
+  data: MarketStockEOD[];
+}
