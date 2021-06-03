5737f3e35f49e0828db26b61575f65e33aff6d77
gitgitWi <johnwi@knou.ac.kr>
2021-06-03 18:06:39 +0900

    Feat: ThemeToggleButton 추가
    - 화면 우하단에 위치
    - 테마 변경에 따라 색상, 문구 변경

---
+++ packages/wiii/frontend/components/atoms/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="noselect">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  border-radius: 15px;
+  cursor: pointer;
+}
+</style>
