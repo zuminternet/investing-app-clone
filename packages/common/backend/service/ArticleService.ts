0df250a148c86cdb616320853b75b3cb41a2786b
Lee Dogyeong <shroad1802@naver.com>
2021-06-04 11:26:47 +0900

    Fix: articleService caching
    * cache 옵션 추가해서 시간지나면 캐시 지워지도록 수정
    
    -

--- packages/common/backend/service/ArticleService.ts
+++ packages/common/backend/service/ArticleService.ts
@@ -2,6 +2,7 @@ import { isValidObjectId, ObjectId } from 'mongoose';
 import { Service } from 'zum-portal-core/backend/decorator/Alias';
 import { Caching } from 'zum-portal-core/backend/decorator/Caching';
 import Article, { ArticleType, ArticleDoc } from '../model/ArticleModel';
+import * as NodeCache from 'node-cache';
 
 interface QueryProps {
   offset?: number;
@@ -9,13 +10,15 @@ interface QueryProps {
   tickers?: string[];
 }
 
+const cache = new NodeCache({ deleteOnExpire: true });
+
 @Service()
 export default class ArticleService {
   private getTickerOption(tickers?: string[]) {
     return tickers ? { tickers: { $in: tickers } } : {};
   }
 
-  @Caching({ ttl: 10, runOnStart: false })
+  @Caching({ ttl: 30, runOnStart: false, cache })
   public async getNews({ offset = 0, limit = 10, tickers }: QueryProps = {}): Promise<ArticleDoc[]> {
     const tickerOption = this.getTickerOption(tickers);
     const query = { type: ArticleType.news, ...tickerOption };
@@ -27,7 +30,7 @@ export default class ArticleService {
       .lean<ArticleDoc[]>();
   }
 
-  @Caching({ ttl: 10, runOnStart: false })
+  @Caching({ ttl: 30, runOnStart: false, cache })
   public async getOpinions({ offset = 0, limit = 10, tickers }: QueryProps = {}): Promise<ArticleDoc[]> {
     const tickerOption = this.getTickerOption(tickers);
     const query = { type: ArticleType.opinions, ...tickerOption };
@@ -39,7 +42,7 @@ export default class ArticleService {
       .lean<ArticleDoc[]>();
   }
 
-  @Caching({ ttl: 10, runOnStart: false })
+  @Caching({ ttl: 30, runOnStart: false, cache })
   public async getArticleById(id: ObjectId | string): Promise<ArticleDoc> {
     if (!isValidObjectId(id)) throw new Error('invalid id');
 
