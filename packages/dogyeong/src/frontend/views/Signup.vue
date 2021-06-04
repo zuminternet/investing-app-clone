3d9f22f93e07766ed662e0b4c1d0c3d4286c26f7
Lee Dogyeong <shroad1802@naver.com>
2021-06-04 17:52:52 +0900

    Feat: Update login,signup,setting markup
    -

--- packages/dogyeong/src/frontend/views/Signup.vue
+++ packages/dogyeong/src/frontend/views/Signup.vue
@@ -1,23 +1,30 @@
 <template>
-  <div>
-    <form>
-      <label>name <input v-model="name" type="text"/></label>
-      <label>email <input v-model="email" type="email"/></label>
-      <label>password <input v-model="password" type="password"/></label>
-      <button type="submit" @click.prevent="onClickSignupBtn">sign up</button>
-    </form>
+  <Layout>
+    <header></header>
+    <main>
+      <form>
+        <input v-model="name" type="text" placeholder="name" />
+        <input v-model="email" type="email" placeholder="email" />
+        <input v-model="password" type="password" placeholder="password" />
+        <button type="submit" @click.prevent="onClickSignupBtn">sign up</button>
+      </form>
+    </main>
     <BottomNav></BottomNav>
-  </div>
+  </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import BottomNav from '@/components/BottomNav/BottomNav.vue';
 import * as authService from '@/services/authService';
+import Layout from '@/components/Layout/Layout.vue';
 
 export default Vue.extend({
+  name: 'Signup',
+
   components: {
     BottomNav,
+    Layout,
   },
 
   data() {
@@ -43,4 +50,32 @@ export default Vue.extend({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+  max-width: 350px;
+  margin: auto;
+
+  input {
+    margin-bottom: 12px;
+    padding: 8px;
+    font-size: 1rem;
+    border-radius: 4px;
+    border: 1px solid var(--border-color);
+  }
+
+  button {
+    display: block;
+    padding: 8px;
+    background-color: var(--primary-color);
+    font-size: 1rem;
+    border-radius: 4px;
+    color: var(--login-text-color);
+  }
+}
+</style>
