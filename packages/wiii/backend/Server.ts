70ff19dfcc4500689d1f0679e81171ae10811515
gitgitWi <johnwi@knou.ac.kr>
2021-06-07 17:39:41 +0900

    Feat: Server.ts에 MongoConnection 추가

--- packages/wiii/backend/Server.ts
+++ packages/wiii/backend/Server.ts
@@ -1,4 +1,18 @@
 import 'reflect-metadata';
 import { appContainer } from './AppContainer';
+import { getMongoConnection } from './db/';
 
-appContainer.listen();
+getMongoConnection()
+  .then((conn) => {
+    appContainer.listen();
+
+    process.on('SIGINT', async () => {
+      try {
+        if (conn) await conn.close();
+      } catch (e) {
+        console.error(e);
+      }
+      process.exit(0);
+    });
+  })
+  .catch((e) => console.error(e));
