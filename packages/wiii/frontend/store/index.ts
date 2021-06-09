728bb5c38d70f680f22e65f2fc2340c5f5d74141
gitgitWi <johnwi@knou.ac.kr>
2021-06-09 18:42:00 +0900

    Feat: Vuex RootStore에 현재 ticker 정보 저장 action 추가

--- packages/wiii/frontend/store/index.ts
+++ packages/wiii/frontend/store/index.ts
@@ -3,11 +3,32 @@ import Vuex, { Store } from 'vuex';
 
 import Theme from '@/store/modules/theme';
 import Reply from '@/store/modules/reply';
+import { RootActions } from '@/store/types';
 export * from '@/store/types';
 
 Vue.use(Vuex);
 
 const store = new Store({
+  state: {
+    ticker: undefined,
+  },
+
+  getters: {
+    getTicker: (state) => state.ticker,
+  },
+
+  mutations: {
+    [RootActions.SET_CURRENT_TICKER]: (state, ticker: string) => {
+      state.ticker = ticker;
+    },
+  },
+
+  actions: {
+    [RootActions.SET_CURRENT_TICKER]: ({ commit }, ticker: string) => {
+      commit(RootActions.SET_CURRENT_TICKER, ticker);
+    },
+  },
+
   modules: {
     Theme,
     Reply,
--- packages/wiii/frontend/store/types.ts
+++ packages/wiii/frontend/store/types.ts
@@ -1,9 +1,16 @@
 export interface RootState {
+  ticker: string;
   //
 }
 
-export enum actionTypes {
-  setCurrentView = 'setCurrentView',
+export enum RootActions {
+  SET_CURRENT_TICKER = 'SET_CURRENT_TICKER',
+}
+
+export enum StoreNames {
+  Theme = 'Theme',
+  Reply = 'Reply',
+  User = 'User',
 }
 
 export enum ChartDataEnums {
