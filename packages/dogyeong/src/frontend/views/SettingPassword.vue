c7ac2e4eee6fcb5affeac5237f2a259bd891dda0
Lee Dogyeong <shroad1802@naver.com>
2021-06-09 09:45:33 +0900

    Feat: Add SettingPassword, SettingUserName component
    -

---
+++ packages/dogyeong/src/frontend/views/SettingPassword.vue
@@ -0,0 +1,57 @@
+<template>
+  <Layout>
+    <Header>
+      <HeaderTitle>비밀번호 변경</HeaderTitle>
+    </Header>
+    <main>
+      <input v-model="password" type="password" />
+      <button @click="changeUserName">save</button>
+    </main>
+    <BottomNav></BottomNav>
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Layout from '@/components/Layout/Layout.vue';
+import { Header, HeaderTitle } from '@/components/Header';
+import BottomNav from '@/components/BottomNav/BottomNav.vue';
+import { changeUserInfo } from '@/services/authService';
+
+export default Vue.extend({
+  name: 'SettingPassword',
+
+  components: { BottomNav, Layout, Header, HeaderTitle },
+
+  data() {
+    return {
+      password: '',
+    };
+  },
+
+  methods: {
+    changeUserName() {
+      changeUserInfo({ password: this.password })
+        .then(() => this.$router.replace('/setting'))
+        .catch(console.error);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+main {
+  padding: 12px;
+
+  input {
+    margin-right: 8px;
+    padding: 4px 8px;
+  }
+
+  button {
+    padding: 4px 8px;
+    border: 1px solid var(--border-color);
+    border-radius: 4px;
+  }
+}
+</style>
