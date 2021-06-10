ee2d8ce6d11bb95f87ef7b1c9583bcf6f1698629
Lee Dogyeong <shroad1802@naver.com>
2021-06-10 17:35:49 +0900

    Feat: Change SSR Config
    -

--- packages/dogyeong/src/backend/facade/HomeFacade.ts
+++ packages/dogyeong/src/backend/facade/HomeFacade.ts
@@ -9,7 +9,7 @@ type SsrHtml = { [key: string]: SsrHtml | string };
 const INIT_CWD = path.join(process.env.INIT_CWD, process.env.BASE_PATH || '');
 const RESOURCES_PATH = path.join(INIT_CWD, 'resources');
 const TEMPLATES_PATH = path.join(RESOURCES_PATH, 'templates');
-const SSR_PATHS = ['/'];
+const SSR_PATHS = ['/', '/news', '/bookmark', '/setting'];
 
 @Facade()
 export class HomeFacade {
