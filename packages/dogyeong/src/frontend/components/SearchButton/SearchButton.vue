60911dd008f3431ab81cc5a856f1828562c24526
Lee Dogyeong <shroad1802@naver.com>
2021-06-10 14:59:07 +0900

    Feat: Add SearchButton component
    -

---
+++ packages/dogyeong/src/frontend/components/SearchButton/SearchButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <RouterLink to="/search">&#128269;</RouterLink>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'SearchButton',
+});
+</script>
+
+<style lang="scss" module></style>
