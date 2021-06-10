cc334b55211a2d35d265575720390ced7e08eaf8
Lee Dogyeong <shroad1802@naver.com>
2021-06-10 15:28:28 +0900

    Feat: Add components for Bookmark
    -

---
+++ packages/dogyeong/src/frontend/components/Bookmark/BookmarkList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <slot />
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BookmarkList',
+});
+</script>
+
+<style lang="scss" module></style>
---
+++ packages/dogyeong/src/frontend/components/Bookmark/BookmarkListItemText.vue
@@ -0,0 +1,13 @@
+<template>
+  <span><slot /></span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BookmarkListItemText',
+});
+</script>
+
+<style lang="scss" module></style>
