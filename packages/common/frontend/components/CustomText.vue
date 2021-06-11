ec3e8812e1e2c6812c9dea3d194df6bd72141b2b
Jaemin Cheon <karl@zuminternet.com>
2021-06-11 10:22:35 +0900

    [style] header button 들 icon 추가 및 스타일링

--- packages/common/frontend/components/CustomText.vue
+++ packages/common/frontend/components/CustomText.vue
@@ -1,5 +1,5 @@
 <template>
-  <span :class="{ 'button-active': buttonActive }">
+  <span :class="{ 'default-text': true, 'button-active': buttonActive, 'app-title': appTitle, 'page-title': pageTitle }">
     <slot></slot>
   </span>
 </template>
@@ -12,13 +12,36 @@ export default {
       type: Boolean,
       default: false,
     },
+
+    appTitle: {
+      type: Boolean,
+      default: false,
+    },
+
+    pageTitle: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
+.default-text {
+  color: var(--text-color);
+}
+
 .button-active {
   color: var(--active-button-color);
+  font-weight: bold;
+}
+
+.app-title {
+  color: var(--title-color);
+  font-weight: 900;
+}
+
+.page-title {
   font-weight: bold;
 }
 </style>
