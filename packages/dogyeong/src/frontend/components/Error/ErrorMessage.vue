c0ff01964102e32c1cc499737e5b7084563c846f
Lee Dogyeong <shroad1802@naver.com>
2021-06-14 15:11:14 +0900

    Feat: Add components for error message
    * 반복되는 에러 관련 부분을 컴포넌트로 분리
    
    -

---
+++ packages/dogyeong/src/frontend/components/Error/ErrorMessage.vue
@@ -0,0 +1,23 @@
+<template>
+  <span :class="$style.message"><slot></slot></span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ErrorMessage',
+});
+</script>
+
+<style lang="scss" module>
+.message {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 22px;
+  color: var(--text-color);
+}
+</style>
