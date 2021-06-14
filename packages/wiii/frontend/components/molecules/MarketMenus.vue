e5bd11e1c6230aa85175a651ce1d9318f031e9a5
gitgitWi <johnwi@knou.ac.kr>
2021-06-14 17:28:47 +0900

    Feat: MarketMenus 컴포넌트 추가
    주식, 지수, 코인 별 화면으로 이동

---
+++ packages/wiii/frontend/components/molecules/MarketMenus.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="area rows-space-between ">
+    <Link class="market-menus-link" v-for="{ name, href } in marketMenuData" :key="name" :name="name" :href="href" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Link from '@/components/atoms/RouterLink.vue';
+
+export default Vue.extend({
+  name: 'MarketMenus',
+
+  components: { Link },
+
+  data() {
+    return {
+      marketMenuData: [
+        { name: '주식', href: '/markets/stocks/' },
+        { name: '지수', href: '/markets/indexes/' },
+        { name: '코인', href: '/markets/coins/' },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.market-menus-link {
+  background-color: transparent;
+
+  &.router-link:hover {
+    background-color: transparent;
+    color: $blue-700;
+    text-decoration-color: $blue-700;
+  }
+}
+</style>
