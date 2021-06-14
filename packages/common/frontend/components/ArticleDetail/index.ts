63db18b7b7eec9a5d1cc4afeb0ce81553eb4bdbb
Lee Dogyeong <shroad1802@naver.com>
2021-06-14 11:16:39 +0900

    Refactor: Add comments
    * 공통 컴포넌트에 주석 추가
    
    -

--- packages/common/frontend/components/ArticleDetail/index.ts
+++ packages/common/frontend/components/ArticleDetail/index.ts
@@ -4,4 +4,13 @@ import ArticleDetailSubInfo from './ArticleDetailSubInfo.vue';
 import ArticleDetailBodyImage from './ArticleDetailBodyImage.vue';
 import ArticleDetailBodyText from './ArticleDetailBodyText.vue';
 
+/**
+ * ArticleDetail
+ *
+ * 뉴스 상세보기 페이지에 사용되는 컴포넌트
+ *
+ * @author dogyeong
+ * @see https://github.com/zuminternet/investing-app-clone/pull/86
+ */
+
 export { ArticleDetailSection, ArticleDetailTitle, ArticleDetailSubInfo, ArticleDetailBodyText, ArticleDetailBodyImage };
