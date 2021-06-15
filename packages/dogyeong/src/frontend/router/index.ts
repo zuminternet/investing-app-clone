ad18bbd17ca2915cf5544507c5973519acdfb76b
Lee Dogyeong <shroad1802@naver.com>
2021-06-15 16:56:39 +0900

    Feat: Add scrollBehavior to router
    -

--- packages/dogyeong/src/frontend/router/index.ts
+++ packages/dogyeong/src/frontend/router/index.ts
@@ -7,7 +7,19 @@ Vue.use(Router);
 export default () => {
   return new Router({
     mode: 'history',
+
     base: process.env.publicPath,
+
     routes,
+
+    // 네이티브같은 스크롤 동작을 위해 scroll 위치 저장 및 초기화
+    // https://router.vuejs.org/kr/guide/advanced/scroll-behavior.html
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else {
+        return { x: 0, y: 0 };
+      }
+    },
   });
 };
