23729e103886fbb2e6fc0b113d7f8a19235b7957
Lee Dogyeong <shroad1802@naver.com>
2021-06-16 17:31:01 +0900

    Feat: Change MarketController
    * 주식,가상화폐,지수 요청 시 값을 조금씩 바꿔서 응답
    
    -

--- packages/dogyeong/src/backend/controller/MarketController.ts
+++ packages/dogyeong/src/backend/controller/MarketController.ts
@@ -2,18 +2,33 @@ import { Controller, GetMapping } from 'zum-portal-core/backend/decorator/Contro
 import { Request, Response } from 'express';
 import { Inject } from 'zum-portal-core/backend/decorator/Alias';
 import MarketService from 'common/backend/service/MarketService';
-import { tickerMap, StockSymbol, IndexSymbol, CryptoSymbol, MarketSymbol, ChartPeriod } from 'common/domain';
+import { EndOfDay, tickerMap, StockSymbol, IndexSymbol, CryptoSymbol, MarketSymbol, ChartPeriod } from 'common/domain';
 
 @Controller({ path: '/api/market' })
 export class MarketController {
   constructor(@Inject(MarketService) private marketService: MarketService) {}
+
+  // 현재가에 난수 더하기
+  private addSalt(eods: EndOfDay[]) {
+    if (!eods.length) return eods;
+
+    const random1 = Math.random();
+    const random2 = Math.random();
+    const salt = (random1 > 0.5 ? 1 : -1) * random2;
+    const index = Math.floor(random1 * eods.length);
+    const { diff, growthRate } = eods[index];
+    eods[index].diff += salt;
+    eods[index].growthRate = ((diff + salt) * growthRate) / diff;
+
+    return eods;
+  }
 
   @GetMapping({ path: ['/indices'] })
   public async getIndices(req: Request, res: Response) {
     try {
       const indexSymbols = Object.keys(tickerMap.index) as IndexSymbol[];
       const indices = await this.marketService.getLatestEOD(indexSymbols);
-      res.json(indices);
+      res.json(this.addSalt(indices));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
@@ -24,7 +39,7 @@ export class MarketController {
     try {
       const coinSymbols = Object.keys(tickerMap.crypto) as CryptoSymbol[];
       const coins = await this.marketService.getLatestEOD(coinSymbols);
-      res.json(coins);
+      res.json(this.addSalt(coins));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
@@ -35,7 +50,7 @@ export class MarketController {
     try {
       const stockSymbols = Object.keys(tickerMap.stock) as StockSymbol[];
       const stocks = await this.marketService.getLatestEOD(stockSymbols);
-      res.json(stocks);
+      res.json(this.addSalt(stocks));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
