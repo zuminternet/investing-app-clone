e29ced4f5907ec88d16b20271931d2ad9d51ab1a
gitgitWi <johnwi@knou.ac.kr>
2021-06-17 15:45:38 +0900

    Feat: 주식 상세 페이지 차트 하단 기업개요, 투자지표 컴포넌트 추가
    - `display: grid`로 정렬
    - api 데이터에 대한 세부 설명이 부족해 데이터 추가하기 어려움..

---
+++ packages/wiii/frontend/components/organisms/StockDetailCompanyInfo.vue
@@ -0,0 +1,148 @@
+<template>
+  <div id="stock-detail-company-info">
+    <Words class="stock-detail-company-subtitle">기업개요</Words>
+    <section class="stock-detail-company">
+      <Words class="stock-detail-company-text head"> 홈페이지 </Words>
+      <Words class="stock-detail-company-text link" @click.native="openUrl"> {{ weburl }} </Words>
+      <Words class="stock-detail-company-text head"> 대표전화 </Words>
+      <Words class="stock-detail-company-text"> {{ phone }} </Words>
+      <Words class="stock-detail-company-text head"> 상장일 </Words>
+      <Words class="stock-detail-company-text"> {{ ipo }} </Words>
+      <Words class="stock-detail-company-text head"> 거래소 </Words>
+      <Words class="stock-detail-company-text"> {{ exchangeKR }} </Words>
+    </section>
+
+    <Words class="stock-detail-company-subtitle">투자지표</Words>
+    <section class="stock-detail-company">
+      <Words class="stock-detail-company-text head"> 최근 결산 연도 EPS </Words>
+      <Words class="stock-detail-company-text"> {{ `${eps.v.toFixed(2)} (${eps.period})` }} </Words>
+      <Words class="stock-detail-company-text head"> 최근 3년 평균 EPS 성장률 </Words>
+      <Words class="stock-detail-company-text"> {{ epsGrowth3Y }} </Words>
+      <Words class="stock-detail-company-text head"> PBR </Words>
+      <Words class="stock-detail-company-text"> {{ pbAnnual }} </Words>
+      <Words class="stock-detail-company-text head"> ROE </Words>
+      <Words class="stock-detail-company-text"> {{ roeRfy }} </Words>
+      <Words class="stock-detail-company-text head"> 1주당 배당금 </Words>
+      <Words class="stock-detail-company-text"> {{ dps }}원 </Words>
+      <Words class="stock-detail-company-text head"> 배당수익률 </Words>
+      <Words class="stock-detail-company-text"> {{ dpsRate }}% </Words>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Words from '@/components/atoms/Words.vue';
+
+export default Vue.extend({
+  name: 'StockDetailCompanyInfo',
+
+  components: { Words },
+
+  props: {
+    weburl: {
+      type: String,
+    },
+    ipo: {
+      type: String,
+    },
+    exchange: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    eps: {
+      type: Object,
+      default: () => {
+        v: 0;
+        period: '';
+      },
+    },
+    epsGrowth3Y: {
+      type: String,
+    },
+    pbAnnual: {
+      type: String,
+    },
+    roiAnnual: {
+      type: String,
+    },
+    roeRfy: {
+      type: String,
+    },
+    peer: {
+      type: Array,
+      default: () => [],
+    },
+    dps: {
+      type: String,
+    },
+    dpsRate: {
+      type: String,
+    },
+  },
+
+  computed: {
+    exchangeKR() {
+      return this.exchange.includes('KOREA EXCHANGE') ? '한국거래소' : '';
+    },
+  },
+
+  methods: {
+    openUrl() {
+      window.open(this.weburl);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+$dark-green: $neon-green;
+
+.stock-detail-company-info {
+  width: 100%;
+}
+
+.stock-detail-company {
+  padding: 10px;
+  display: grid;
+  grid-template-columns: 2fr 3fr 2fr 3fr;
+  grid-auto-rows: minmax(1.2rem, auto);
+  justify-content: center;
+
+  &-subtitle {
+    margin-top: 5px;
+    padding: 5px;
+    width: max-content;
+
+    font-size: 1.1rem;
+    text-align: left;
+    color: $grey-700;
+
+    .dark & {
+      color: $dark-green;
+    }
+  }
+
+  &-text {
+    font-size: 0.9rem;
+    color: $grey-900;
+    text-align: left;
+
+    &.head {
+      font-weight: bolder;
+    }
+
+    &.link {
+      cursor: pointer;
+      font-style: oblique;
+      text-decoration: underline;
+    }
+
+    .dark & {
+      color: rgba($dark-green, 0.6);
+    }
+  }
+}
+</style>
--- packages/wiii/frontend/components/templates/Markets/StocksDetail.vue
+++ packages/wiii/frontend/components/templates/Markets/StocksDetail.vue
@@ -10,16 +10,9 @@
     <PriceSummary v-bind="{ ...priceSummary }" />
     <Chart class="card" :typeName="`stock`" :apiType="`es`" :ticker="ticker" />
 
-    <!-- 상세정보 표시 영역 -->
-    <section>
-      <Words> {{ weburl }} </Words>
-      <Words> 상장일: {{ ipo }} </Words>
-      <Words> 전화: {{ phone }} </Words>
-      <Words> 최근 3년 EPS 성장률 {{ epsGrowth3Y }} </Words>
-      <Words> 연간 BPS {{ pbAnnual }} </Words>
-      <Words> 연간 ROI {{ roiAnnual }} </Words>
-      <Words> 최근 연간 ROE {{ roeRfy }} </Words>
-    </section>
+    <CompanyInfo v-bind="{ ...companyInfo }" />
+
+    <!-- 뉴스 일부 표시 -->
 
     <ReplySection :ticker="ticker" />
   </div>
@@ -32,16 +25,34 @@ import { createNamespacedHelpers } from 'vuex';
 import Words from '@/components/atoms/Words.vue';
 import Chart from '@/components/molecules/Chart.vue';
 import PriceSummary from '@/components/organisms/StockDetailPriceSummary.vue';
+import CompanyInfo from '@/components/organisms/StockDetailCompanyInfo.vue';
 import ReplySection from '@/components/organisms/ReplySection.vue';
 import { StoreNames } from '@/store';
 
 const { mapState } = createNamespacedHelpers(StoreNames.Market);
+const phoneReplacer = (_phone: string) => {
+  const phone = _phone.split('.')[0];
+  const len = phone.length;
+  /**
+   * 10자리: 82 2 200 1114
+   * 11자리_1: 82 31 200 1114
+   * 11자리_2: 82 2 6100 2114
+   * 12자리: 82 31 2000 1114
+   */
+  let phoneReg;
+  if (len === 10) phoneReg = /(82)(\d{1})(\d{3})(\d{4})/;
+  if (len === 11) phoneReg = phone[2] === '2' ? /(82)(\d{1})(\d{4})(\d{4})/ : /(82)(\d{2})(\d{3})(\d{4})/;
+  if (len === 12) phoneReg = /(82)(\d{2})(\d{4})(\d{4})/;
+
+  return phone.replace(phoneReg, (_, n1, n2, n3, n4) => `+${n1}) ` + [`0${n2}`, n3, n4].join('-'));
+};
 
 export default Vue.extend({
   components: {
     Words,
     Chart,
     PriceSummary,
+    CompanyInfo,
     ReplySection,
   },
 
@@ -54,18 +65,9 @@ export default Vue.extend({
 
   data() {
     return {
-      ipo: undefined,
-      phone: undefined,
-      weburl: undefined,
       logo: undefined,
-
-      peer: [],
-      epsGrowth3Y: undefined,
-      pbAnnual: undefined,
-      roeRfy: undefined,
-      roiAnnual: undefined,
-
       priceSummary: {},
+      companyInfo: {},
     };
   },
 
@@ -93,30 +95,40 @@ export default Vue.extend({
     },
   },
 
-  mounted() {
+  beforeMount() {
     const {
-      metric: { metric },
+      metric: {
+        metric,
+        series: { annual },
+      },
       peer,
-      profile: { ipo, phone, weburl, marketCapitalization, logo },
+      profile: { ipo, phone, weburl, marketCapitalization, logo, exchange },
     } = this.stockOverviews[this.ticker];
 
-    const { epsGrowth3Y, pbAnnual, roeRfy, roiAnnual } = metric;
+    const { epsGrowth3Y, pbAnnual, roeRfy, roiAnnual, dividendPerShareAnnual, dividendYieldIndicatedAnnual } = metric;
+    const { eps } = annual;
 
-    this.ipo = ipo;
-    this.phone = phone.split('.')[0];
-    this.weburl = weburl;
     this.logo = logo;
-
-    this.peer = peer;
-    this.epsGrowth3Y = epsGrowth3Y.toFixed(2);
-    this.pbAnnual = pbAnnual.toFixed(2);
-    this.roeRfy = roeRfy.toFixed(2);
-    this.roiAnnual = roiAnnual.toFixed(2);
 
     this.priceSummary = {
       marketCapitalization: marketCapitalization.toLocaleString(),
       week52High: metric['52WeekHigh'].toLocaleString(),
       week52Low: metric['52WeekLow'].toLocaleString(),
+    };
+
+    this.companyInfo = {
+      ipo,
+      phone: phoneReplacer(phone),
+      weburl,
+      exchange,
+      peer,
+      eps: eps[0],
+      epsGrowth3Y: epsGrowth3Y.toFixed(2),
+      pbAnnual: pbAnnual.toFixed(2),
+      roeRfy: roeRfy.toFixed(2),
+      roiAnnual: roiAnnual.toFixed(2),
+      dps: dividendPerShareAnnual.toLocaleString(),
+      dpsRate: dividendYieldIndicatedAnnual.toFixed(2),
     };
   },
 });
