a51d8d25b904d329ad5ca416f6ddc1ed81428c92
Lee Dogyeong <shroad1802@naver.com>
2021-06-17 15:38:14 +0900

    Feat: Add feature that add random value to historicalData
    * MarketController에서 차트 데이터에 난수 더해주는 기능 추가
    
    -

--- packages/dogyeong/src/backend/controller/MarketController.ts
+++ packages/dogyeong/src/backend/controller/MarketController.ts
@@ -2,20 +2,36 @@ import { Controller, GetMapping } from 'zum-portal-core/backend/decorator/Contro
 import { Request, Response } from 'express';
 import { Inject } from 'zum-portal-core/backend/decorator/Alias';
 import MarketService from 'common/backend/service/MarketService';
-import { EndOfDay, tickerMap, StockSymbol, IndexSymbol, CryptoSymbol, MarketSymbol, ChartPeriod } from 'common/domain';
+import {
+  EndOfDay,
+  tickerMap,
+  StockSymbol,
+  IndexSymbol,
+  CryptoSymbol,
+  MarketSymbol,
+  ChartPeriod,
+  HistoricalData,
+} from 'common/domain';
 
 @Controller({ path: '/api/market' })
 export class MarketController {
   constructor(@Inject(MarketService) private marketService: MarketService) {}
 
-  // 현재가에 난수 더하기
-  private addSalt(eods: EndOfDay[]) {
-    if (!eods.length) return eods;
-
+  // 난수를 생성하는 함수
+  // bcrypt에서 패스워드에 추가하는 임의의 값을 salt라고 부르는 것에서 따와서 난수를 Salt라고 명명
+  private createSalt() {
     const random1 = Math.random();
     const random2 = Math.random();
     const salt = (random1 > 0.5 ? 2 : -2) * random2;
-    const index = Math.floor(random1 * eods.length);
+    return salt;
+  }
+
+  // EOD에 난수 더하기
+  private addSaltToEOD(eods: EndOfDay[]) {
+    if (!eods.length) return eods;
+
+    const salt = this.createSalt();
+    const index = Math.floor(Math.random() * eods.length);
     const { diff, growthRate } = eods[index];
     eods[index].close = +(eods[index].close + salt).toFixed(2);
     eods[index].diff = +(eods[index].diff + salt).toFixed(2);
@@ -24,12 +40,20 @@ export class MarketController {
     return eods;
   }
 
+  // 차트 데이터에 난수 더하기
+  private addSaltToHistoricalData(historicalData: HistoricalData) {
+    const salt = this.createSalt();
+    const lastCandle = historicalData.data[0];
+    if (lastCandle) lastCandle.close = +(lastCandle.close + salt).toFixed(2);
+    return historicalData;
+  }
+
   @GetMapping({ path: ['/indices'] })
   public async getIndices(req: Request, res: Response) {
     try {
       const indexSymbols = Object.keys(tickerMap.index) as IndexSymbol[];
       const indices = await this.marketService.getLatestEOD(indexSymbols);
-      res.json(this.addSalt(indices));
+      res.json(this.addSaltToEOD(indices));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
@@ -40,7 +64,7 @@ export class MarketController {
     try {
       const coinSymbols = Object.keys(tickerMap.crypto) as CryptoSymbol[];
       const coins = await this.marketService.getLatestEOD(coinSymbols);
-      res.json(this.addSalt(coins));
+      res.json(this.addSaltToEOD(coins));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
@@ -51,7 +75,7 @@ export class MarketController {
     try {
       const stockSymbols = Object.keys(tickerMap.stock) as StockSymbol[];
       const stocks = await this.marketService.getLatestEOD(stockSymbols);
-      res.json(this.addSalt(stocks));
+      res.json(this.addSaltToEOD(stocks));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
@@ -74,7 +98,7 @@ export class MarketController {
       const { symbol } = req.params;
       const { period } = req.query;
       const chart = await this.marketService.getHistoricalData(symbol as MarketSymbol, period as ChartPeriod);
-      res.json(chart);
+      res.json(this.addSaltToHistoricalData(chart));
     } catch (err) {
       res.status(500).json({ err: err.message ?? err });
     }
