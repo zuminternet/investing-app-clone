3a6acdf9de8efe4e3643c554e2f542a68637fa53
gitgitWi <johnwi@knou.ac.kr>
2021-06-18 14:40:49 +0900

    Fix: controller 라우팅 처리
    - client에서 subpath 접속시 404 에러 해결

--- packages/wiii/backend/facade/HomeFacade.ts
+++ packages/wiii/backend/facade/HomeFacade.ts
@@ -3,12 +3,10 @@ import { BundleRenderer, createBundleRenderer } from 'vue-server-renderer';
 import { Yml } from 'zum-portal-core/backend/decorator/Yml';
 
 const path = require('path');
-const fs = require('fs');
 
 type SsrHtml = { [key: string]: SsrHtml | string };
 const RESOURCES_PATH = path.join(process.env.INIT_CWD, 'resources');
-const TEMPLATES_PATH = path.join(RESOURCES_PATH, 'templates');
-const SSR_PATHS = ['/'];
+const SSR_PATHS = ['/', '/markets', '/news', '/user'];
 
 @Facade()
 export class HomeFacade {
@@ -26,15 +24,16 @@ export class HomeFacade {
     // SSR 번들 렌더러 생성
     this._emptyHtml = `<div id="app"></div>`;
     const bundle = require(path.join(process.env.INIT_CWD, application.renderer));
-    this.renderer = createBundleRenderer(bundle, { runInNewContext: false, clientManifest: clientManifest });
+    this.renderer = createBundleRenderer(bundle, { runInNewContext: false, clientManifest });
   }
 
   public async renderHtml(): Promise<void> {
-    for (let path of SSR_PATHS) {
+    for (const path of SSR_PATHS) {
       this.ssrHtml[path] = this.ssrHtml[path] || {};
 
       try {
-        this.ssrHtml[path] = process.env.NODE_ENV === 'production' ? await this.renderer.renderToString() : this._emptyHtml;
+        this.ssrHtml[path] =
+          process.env.NODE_ENV === 'production' ? await this.renderer.renderToString({ path }) : this._emptyHtml;
       } catch (e) {
         console.error(e);
       }
