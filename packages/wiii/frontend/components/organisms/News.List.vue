c5024d0c41a44f3fef9549f4f5bb61b2c28703d5
gitgitWi <johnwi@knou.ac.kr>
2021-06-18 17:58:37 +0900

    Feat: 뉴스 기능 Frontend Component 구현 중
    - 뉴스 메인 페이지 시장 뉴스 목록 구현 중
    - News.List.Card 스타일링 필요

---
+++ packages/wiii/frontend/components/organisms/News.List.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="news-main-list" class="section">
+    <Card v-for="{ category, datetime, headline, id, image, source, summary, url } in newsList" :key="id">
+      {{ headline }}
+
+      {{ summary }}
+    </Card>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Card from '@/components/molecules/News.List.Card.vue';
+
+export default Vue.extend({
+  name: 'NewsList',
+
+  components: { Card },
+
+  props: {
+    newsList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+#news-main-list {
+  width: 100%;
+  min-height: 300px;
+}
+</style>
