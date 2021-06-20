c88728643c82f122108d2cfc7e63fdeb1e850e88
gitgitWi <johnwi@knou.ac.kr>
2021-06-21 01:56:33 +0900

    Feat: 뉴스 카드 SCSS 스타일링
    - News.List.Card 컴포넌트 CSS 적용
    
    ## TODO
    
    - 클릭시 상세페이지 -> 모달 적용
    - 북마크, 댓글

--- packages/wiii/frontend/components/molecules/News.List.Card.vue
+++ packages/wiii/frontend/components/molecules/News.List.Card.vue
@@ -1,12 +1,13 @@
 <template>
-  <article class="card news-list-card">
-    <Words>{{ category }} </Words>
-    <LazyImages :src="image" :alt="headline" class="news-list-card-thumbnail" />
+  <article class="news-list-card card">
     <Words class="news-list-card-title">{{ headline }} </Words>
-    <Words>{{ summary }} </Words>
-    <Words>{{ url }} </Words>
-    <Words>{{ source }} </Words>
-    <Words>{{ dateString }} </Words>
+    <LazyImages :src="image" :alt="headline" class="news-list-card-thumbnail" />
+    <Words class="news-list-card-summary">{{ summary }} </Words>
+    <div class="news-list-card-tags noselect">
+      <Words class="news-list-card-tag news-list-card-category" :class="categoryColor"> {{ category.toUpperCase() }} </Words>
+      <Words class="news-list-card-tag news-list-card-source"> {{ source }} </Words>
+      <Words class="news-list-card-tag news-list-card-date">{{ dateString }} </Words>
+    </div>
   </article>
 </template>
 
@@ -17,6 +18,11 @@ import LazyImages from '@/components/atoms/LazyImages.vue';
 import Card from '@/components/molecules/Card.vue';
 import { getDateString } from '../../../domain/date';
 
+const categoryColors = {
+  'top news': 'top',
+  business: 'business',
+};
+
 export default Vue.extend({
   name: 'NewsListCard',
 
@@ -25,21 +31,26 @@ export default Vue.extend({
   props: {
     id: {
       type: Number,
+      required: true,
     },
     category: {
       type: String,
+      default: '',
     },
     headline: {
       type: String,
+      required: true,
     },
     image: {
       type: String,
     },
     source: {
       type: String,
+      default: '',
     },
     summary: {
       type: String,
+      default: '',
     },
     datetime: {
       type: Number,
@@ -54,12 +65,24 @@ export default Vue.extend({
       /** datetime; UNIX timestamp 이므로 1_000 곱해줘야 함 */
       return getDateString(this.datetime * 1000);
     },
+
+    categoryColor() {
+      return categoryColors[this.category];
+    },
   },
 });
 </script>
 
 <style lang="scss" scoped>
 .news-list-card {
+  &.card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 10px;
+  }
+
   &-title {
     font-weight: bold;
     font-size: 1.2rem;
@@ -67,13 +90,48 @@ export default Vue.extend({
 
   &-thumbnail {
     width: 100%;
-    margin: 10px;
+    margin: 10px 0;
     border-radius: $border-radius-10;
 
     .loading {
       min-width: 100px;
       min-height: 100px;
     }
+  }
+
+  &-tags {
+    padding-top: 15px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  &-tag {
+    width: max-content;
+    overflow: visible;
+    padding: 0 10px;
+    border-radius: $border-radius-10;
+    font-size: 0.8rem;
+    color: $grey-300;
+    cursor: default;
+  }
+
+  &-category {
+    &.top {
+      background-color: $blue-900;
+    }
+
+    &.business {
+      background-color: $red-700;
+    }
+  }
+
+  &-source {
+    background-color: $shallow-blue;
+  }
+
+  &-date {
+    background-color: $grey-700;
   }
 }
 </style>
