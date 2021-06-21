22fac2aae647d2adf2ca43790efeeae1fc1e5615
Jaemin Cheon <karl@zuminternet.com>
2021-06-21 18:51:49 +0900

    [feature] 종목 상세페이지, 마켓 페이지에 실시간 등락 반영

--- packages/karl/frontend/views/ItemDetail.vue
+++ packages/karl/frontend/views/ItemDetail.vue
@@ -218,8 +218,13 @@ export default {
     const email = this.userInfo.userEmail;
     const tickers = [symbols];
 
+    const fetcher = this.getItemDetail.bind(this, { symbols, email, name });
+
     this.getItemDetail({ symbols, email, name }).then(() => {
       this.setItemDetailIsLoading(false);
+      this.intervalForItemDetail = setInterval(function() {
+        fetcher();
+      }, 2000);
     });
 
     this.getNews({ offset: 0, limit: 20, tickers }).then(() => {
@@ -229,6 +234,12 @@ export default {
     this.getAnalyses({ offset: 0, limit: 20, tickers }).then(() => {
       this.setAnalysesIsLoading(false);
     });
+  },
+
+  beforeDestroy() {
+    if (this.intervalForItemDetail) {
+      clearInterval(this.intervalForItemDetail);
+    }
   },
 };
 </script>
